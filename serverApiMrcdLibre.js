965ad8fe0d42f30b627fb7906b751f58ba2ab88b
jquinteromo <qjaider1111@gmail.com>
2025-08-22 12:26:49 -0500

    first commit

---
+++ serverApiMrcdLibre.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+
+app.get('/api/products', async (req, res) => {
+  try {
+    const response = await fetch('https://api.mercadolibre.com/sites/MCO/search?q=silla+oficina');
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: 'Error al obtener productos' });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Servidor corriendo en Render');
+});
