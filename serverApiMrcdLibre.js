62eab24290d78fc2059f6316b03c293f9839823c
jquinteromo <qjaider1111@gmail.com>
2025-08-22 13:56:32 -0500

    add user agent a header request

--- serverApiMrcdLibre.js
+++ serverApiMrcdLibre.js
@@ -7,13 +7,23 @@ app.use(cors());
 
 app.get('/api/products', async (req, res) => {
   try {
-    const { data } = await axios.get('https://api.mercadolibre.com/sites/MCO/search?q=silla+oficina');
+    const { data } = await axios.get(
+      'https://api.mercadolibre.com/sites/MCO/search?q=silla+oficina',
+      {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (compatible; MrcdLibreBot/1.0)',
+        }
+      }
+    );
+    console.log('✅ Respuesta recibida');
     res.json(data);
   } catch (err) {
-    console.error('Error en axios:', err.message);
+    console.error('❌ Error en axios:', err.response?.status, err.message);
     res.status(500).json({ error: 'Error al obtener productos', detalle: err.message });
   }
 });
+
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Servidor corriendo en Render');
